547ffd73af247376006b8d8ac9d0bf5b590446fc
GBFerro <giovani.ferro@usp.br>
2023-04-20 15:28:30 -0300

    Add project files.

---
+++ projAPIEntityFramework/Controllers/ClientsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projAPIEntityFramework.Data;
+using projAPIEntityFramework.Models;
+
+namespace projAPIEntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly projAPIEntityFrameworkContext _context;
+
+        public ClientsController(projAPIEntityFrameworkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClient()
+        {
+            if (_context.Client == null)
+            {
+                return NotFound();
+            }
+            return await _context.Client.Include(c => c.Address).ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            if (_context.Client == null)
+            {
+                return NotFound();
+            }
+            //var client = await _context.Client.FindAsync(id);
+            var client = await _context.Client.Include(c => c.Address).Where(client => client.Id == id).FirstOrDefaultAsync();
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            if (_context.Client == null)
+            {
+                return Problem("Entity set 'projAPIEntityFrameworkContext.Client'  is null.");
+            }
+            var address = await _context.Address?.Where(a => a.Id == client.Address.Id).FirstOrDefaultAsync();
+            //var address = await _context.Address.FindAsync(client.Address.Id);
+            client.Address = address;
+            _context.Client.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            if (_context.Client == null)
+            {
+                return NotFound();
+            }
+            var client = await _context.Client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Client.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return (_context.Client?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ projAPIEntityFramework/Data/projAPIEntityFrameworkContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using projAPIEntityFramework.Models;
+
+namespace projAPIEntityFramework.Data
+{
+    public class projAPIEntityFrameworkContext : DbContext
+    {
+        public projAPIEntityFrameworkContext (DbContextOptions<projAPIEntityFrameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<projAPIEntityFramework.Models.Animal> Animal { get; set; } = default!;
+
+        public DbSet<projAPIEntityFramework.Models.Client>? Client { get; set; }
+
+        public DbSet<projAPIEntityFramework.Models.Address> Address { get; set; }
+    }
+}
